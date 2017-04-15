a7799d1c0085fd8c02838044dcab24cbe5837369
Kelby Ludwig <kelby.ludwig@praetorian.com>
2017-04-15 17:36:46 -0500

    adding doctests to test script

--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "running ecdsa tests..."
-sage mage/tests/ecdsa_test.sage
-echo "running matrix utils tests..."
-sage mage/tests/matrix_utils_test.sage
+sage mage/tests/ecdsa_test.sage && \
+echo "running ecdsa doctests..." && \
+sage -t mage/ecdsa.py && \
+echo "running matrix utils tests..." && \
+sage mage/tests/matrix_utils_test.sage && \
+echo "running matrix utils doctests..." && \
+sage -t mage/matrix_utils.py
