6ebbada31d0515836e61376309e8c893143a8977
Kelby Ludwig <kelby.ludwig@praetorian.com>
2017-04-15 21:03:30 -0500

    updated test script

--- bin/test.sh
+++ bin/test.sh
@@ -6,4 +6,6 @@ sage -t mage/ecdsa.py && \
 echo "running matrix utils tests..." && \
 sage mage/tests/matrix_utils_test.sage && \
 echo "running matrix utils doctests..." && \
-sage -t mage/matrix_utils.py
+sage -t mage/matrix_utils.py && \
+echo "running vector utils doctests..." && \
+sage -t mage/vector_utils.py
